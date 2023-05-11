9af4ea00ebb7f8d897cf9462d9a427d059953aed
Diana Doan <122428383+d2doan@users.noreply.github.com>
2023-05-11 11:46:09 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,37 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission
+
+if ![[ -f `find ListExamples.java` ]]
+then 
+    echo "Please review the format of your submission. Our autograder could not find the right file."
+    exit
+else
+then
+    cp student-submission/*.java grading-area
+    #cp list-examples-grader/TestListExamples.java grading-area #don't know if this will work
+    cat grading-area
+fi
+#hopefully student java file and my tests are in grading-area
+
+#to compile the java files
+cd ./grading-area
+
+javac -cp $CPATH ListExamples.java TestListsExamples.java 2>errors.txt
+if ![[ $? == 0 ]]
+then
+    echo "Oh no"
+    cat errors.txt
+    exit
+fi 
+
+
+java -cp $CPATH org.junit.runner.JUnitCore ListExamples 
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
